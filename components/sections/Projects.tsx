427157084af6c813d8a0bf601f3cef73abcb1009
prashant-80 <prashantsingh200502@gmail.com>
2024-08-29 18:08:06 +0530

    finisshing

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -8,7 +8,7 @@ import { FaGithub } from 'react-icons/fa';
 export default function Projects() {
 {/* Projects Section */}
 return (
-<section id="projects" className="py-20">
+<section id="projects" className="py-20  px-7">
 <h2 className="text-3xl font-bold mb-8 text-center">Projects</h2>
 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
   {projects.map((project, index) => (
